7c0cfe93c841d7c6f809622c535503b156eadd9b
Perazzojoao <perazzojoao@gmail.com>
2024-09-18 23:28:42 -0300

    Modificação de base_url

--- frontend/my-app/apiConfig.ts
+++ frontend/my-app/apiConfig.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const BASE_URL = process.env.API_URL || "http://192.168.68.114:8001/api/v2";
+const BASE_URL = process.env.API_URL || "https://tasker-api.perazzojoao.online/api/v2";
 const BASE_FRONT_URL = "http://localhost:3000";
 
 export { BASE_URL, BASE_FRONT_URL };
