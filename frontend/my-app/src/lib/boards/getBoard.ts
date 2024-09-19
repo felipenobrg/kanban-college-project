54540651ae5650f75e91d3f6c95e53d779ac01b0
Perazzojoao <perazzojoao@gmail.com>
2024-09-19 00:56:43 -0300

    Implementação de como fazer a autentificação

--- frontend/my-app/src/lib/boards/getBoard.ts
+++ frontend/my-app/src/lib/boards/getBoard.ts
@@ -1,10 +1,15 @@
 import axios from 'axios'
 import { BASE_URL } from '../../../apiConfig';
+import { getSession } from "next-auth/react"
+import Session from 'next-auth';
 
 export default async function GetBoard() {
     try {
+        const session = await getSession();
+        const token = session?.user?.image; // Deveria ter acesso ao token!!
         const response = await axios.get(`${BASE_URL}/boards`,{
-            withCredentials: true
+            withCredentials: true,
+            headers: { Authorization: `Bearer ${token}` }
         });
         return response.data;
     } catch(error) {
