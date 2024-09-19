87169a22ae95ca60d70e1fd1a901e604b40c6c17
Perazzojoao <perazzojoao@gmail.com>
2024-09-18 22:10:26 -0300

    Atualização de base url

--- frontend/my-app/apiConfig.ts
+++ frontend/my-app/apiConfig.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://tasker-ijmskkp2ia-uc.a.run.app/api/v2";
+const BASE_URL = process.env.API_URL || "http://localhost:8001/api/v2";
 const BASE_FRONT_URL = "http://localhost:3000";
 
 export { BASE_URL, BASE_FRONT_URL };
