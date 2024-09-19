035add0d3a33a437c713c13535b2bec765320862
Perazzojoao <perazzojoao@gmail.com>
2024-09-18 22:38:13 -0300

    Atualização de base url

--- frontend/my-app/apiConfig.ts
+++ frontend/my-app/apiConfig.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.API_URL || "http://localhost:8001/api/v2";
+const BASE_URL = process.env.API_URL || "http://192.168.68.114:8001/api/v2";
 const BASE_FRONT_URL = "http://localhost:3000";
 
 export { BASE_URL, BASE_FRONT_URL };
